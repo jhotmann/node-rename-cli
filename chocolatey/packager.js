6eb801c55d83b1cd04e3c9e3d648c0521fd78f60
Jordan Hotmann <jhot@protonmail.com>
2020-05-11 09:28:51 -0600

    Fix path to build output

--- chocolatey/packager.js
+++ chocolatey/packager.js
@@ -6,5 +6,5 @@ const packageJson = require('../package.json');
 
 let results = nunjucks.render('chocolatey/rename-cli.nuspec.html', packageJson);
 fs.writeFileSync('chocolatey/rename-cli/rename-cli.nuspec', results, 'utf8');
-fs.copyFileSync('bin/rename-cli-win.exe', 'chocolatey/rename-cli/tools/rname.exe');
+fs.copyFileSync('bin/rename-cli.exe', 'chocolatey/rename-cli/tools/rname.exe');
 fs.copyFileSync('license', 'chocolatey/rename-cli/tools/LICENSE.txt');
